4addda032df6c1e79df99c8bacfecad9fadd18ef
kotodzukuri <kotodzukuri@outlook.jp>
2025-07-11 11:54:45 +0900

    fix: 修正課題 holiday.js（祝日16個対応）

--- kadai_010/holiday.js
+++ kadai_010/holiday.js
@@ -1,16 +1,15 @@
 const holidays = [
-  "正月", "成人の日", "建国記念日", "天皇誕生日", "春分の日",
-  "昭和の日", "憲法記念日", "みどりの日", "こどもの日"
+  "正月", "成人の日", "建国記念の日", "天皇誕生日", "春分の日", "昭和の日",
+  "憲法記念日", "みどりの日", "こどもの日", "海の日", "山の日", "敬老の日",
+  "秋分の日", "スポーツの日", "文化の日", "勤労感謝の日"
 ];
 
-// for文で表示
-console.log("【for文で出力】");
+console.log("＜for文の出力＞");
 for (let i = 0; i < holidays.length; i++) {
   console.log(holidays[i]);
 }
 
-// while文で表示
-console.log("【while文で出力】");
+console.log("＜while文の出力＞");
 let j = 0;
 while (j < holidays.length) {
   console.log(holidays[j]);
